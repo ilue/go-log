1cfb3f316cd2a1aaea1dd1b659735fb33e7dec66
juli <ilue26@gmail.com>
2020-03-20 09:29:19 +0800

    Working on

--- writer.go
+++ writer.go
@@ -14,7 +14,7 @@ func SyncWriter(w io.Writer) io.Writer {
 	return &syncWriter{w: w}
 }
 
-func (s *syncWriter) Write(p []byte) (int, err) {
+func (s *syncWriter) Write(p []byte) (int, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	return s.w.Write(p)
